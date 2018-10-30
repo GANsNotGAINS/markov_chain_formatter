82a1ac062e51b66281d771a457498f5ba8d9ca3c
SrinidhiPalwayi <srinidhipalwayi@lawn-143-215-114-218.lawn.gatech.edu>
2018-10-30 18:45:06 -0400

    use string to convert

--- sparse_to_list.py
+++ sparse_to_list.py
@@ -1,28 +1,31 @@
 from numpy import array
 from scipy.sparse import csr_matrix
+import matplotlib.pyplot as plt
+import numpy as np
 # create dense matrix
-A = array([[1, 2, 3, 0, 0, 0], [0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0]])
+img = plt.imread('test.png')
+row, col, channels = img.shape
+img = np.transpose(img, [1,0,2])
 
-S = csr_matrix(A)
-print(S.indices)
-print(S.indptr)
+print(img.shape)
 
 
 vector_int = dict()
 markov_chain_input = list()
 counter=0
 #convert to sparse matrix, then place into a dict
-for column in A.T:
-    sparse = csr_matrix(column)
-    key = tuple(sparse.indices)
+for column in img:
+    key = str(column)
+    #print(key)
     if key in vector_int:
        markov_chain_input.append(vector_int[key])
     else:
         vector_int[key] = counter
         markov_chain_input.append(counter)
         counter+=1
-print(vector_int)
-print(markov_chain_input)
+
+#print(vector_int)
+print(len(markov_chain_input))
 
 
 
